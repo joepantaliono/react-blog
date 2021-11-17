223ed28688b5c964508e4cbc39cf9eaf00a0ea4e
Joseph Pantaliono <pantalionojoseph@gmail.com>
2021-11-17 14:33:27 -0600

    added reusable BlogList

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,18 @@
 import React from 'react'
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: "My first post", body: "lorem ipsum", author: "joe pantaliono", id: 1 },
-        { title: "My second post. My second post!", body: "lorem ipsum", author: "joe pantaliono", id: 2 },
-        { title: "My third post", body: "lorem ipsum", author: "joe pantaliono", id: 3 }
+        { title: "My second post. My second post!", body: "lorem ipsum", author: "john smith", id: 2 },
+        { title: "My third post", body: "lorem ipsum", author: "joe pantaliono", id: 3 },
     ])
 
     return (
-        <div className="home-content">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>{blog.author}</p>
-                </div>
-            ))}
+        <div className="Home">
+            <BlogList blogs={blogs} title="All Blogs" />
+            <BlogList blogs={blogs.filter(blog => blog.author === "joe pantaliono")} title="Joe's Blogs" />
         </div>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,15 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
 
 const Navbar = () => {
     return (
-        <h1>hello</h1>
-    )
+        <nav className="navbar">
+            <h1>Joe Pantaliono's Blog</h1>
+            <div className="links">
+                <p><a href="/">Home</a></p>
+                <p><a href="/">New Post</a></p>
+            </div>
+        </nav>
+    )   
 }
 
 export default Navbar
