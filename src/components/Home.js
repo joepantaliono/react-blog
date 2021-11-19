ca07685fb2cd5b523e5e4e406581be8fb6ae6bfc
Joseph Pantaliono <pantalionojoseph@gmail.com>
2021-11-19 11:29:46 -0600

    add fetch hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,15 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-        .then(res => {
-            return res.json();
-        })
-        .then(data => {
-            setBlogs(data);
-        })
-    }, []);
+    const { data: blogs, isLoading, error } = useFetch('http://localhost:8000/blogs')
 
     return (
         <div className="Home">
+            {error && <div>{error}</div>}
+            {isLoading && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="Recent posts" />}
         </div>
     )
