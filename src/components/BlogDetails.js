9a2c11a269070f2225e054f2dc770b8595c55a33
Joseph Pantaliono <pantalionojoseph@gmail.com>
2021-11-19 21:41:18 -0600

    add POST for new blog

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import { useParams } from 'react-router'
+import useFetch from './useFetch';
 
 const BlogDetails = () => {
     const { id } = useParams();
+    const { data: blog, error, isLoading } = useFetch('http://localhost:8000/blogs/' + id);
 
     return (
         <div className="blog-details">
-            <h2>Blog Details - {id}</h2> 
+            { isLoading && <div>Loading...</div> }
+            { error && <div>{ error }</div> }
+            { blog && (
+                <article>
+                    <h2>{ blog.title }</h2>
+                    <div>{ blog.body }</div>
+                </article>
+            )}
         </div>
-    )
+    );
 }
 
 export default BlogDetails
--- src/components/Create.js
+++ src/components/Create.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+        
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log('new blog added')
+        })
+    }
+
     return (
         <div className="Create">
             <h2>Add a new blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog title:</label>
+                <input type="text" required
+                onChange={(e) => {
+                    setTitle(e.target.value)
+                }}></input>
+                <label>Blog author:</label>
+                <input type="text" required
+                onChange={(e) => {
+                    setAuthor(e.target.value)
+                }}></input>
+                <label>Blog body:</label>
+                <textarea required
+                onChange={(e) => {
+                    setBody(e.target.value)
+                }}></textarea>
+                <button type="submit">Add blog</button>
+            </form>
         </div>
     )
 }
